ec059d18d409dc09d07970eb7f3716774e7c61c1
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2020-11-19 20:05:46 -0300

    Adicionando Toast na aplicação

--- src/hooks/AuthContext.tsx
+++ src/hooks/AuthContext.tsx
@@ -1,9 +1,9 @@
-import React, { createContext, useCallback, useState } from 'react';
+import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
 
 interface AuthState {
   token: string;
-  user: object;
+  user: any;
 }
 
 interface SignInCredentials {
@@ -12,8 +12,9 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: any;
   signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -44,11 +45,28 @@ const AuthProvider: React.FC = ({ children }) => {
     setData({ token, user });
   }, []);
 
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@GoBarber:token');
+    localStorage.removeItem('@GoBarber:user');
+
+    setData({} as AuthState);
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
 };
 
-export { AuthContext, AuthProvider };
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -15,7 +15,7 @@ import { Container, Content, Background } from './styles';
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = useCallback(async (data: object) => {
+  const handleSubmit = useCallback(async (data: any) => {
     try {
       formRef.current?.setErrors({});
 
